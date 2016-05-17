48fc2588d333697564b06b4bc0188a41fc8d586d
AbandonZHANG <441524586@qq.com>
2016-05-17 14:38:32 +0800

    first version

---
+++ source/js/jquery.fancybox.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('img').each(function() {
+    if ($(this).parent().hasClass('fancybox')) return;
+    if ($(this).hasClass('nofancybox')) return;
+    var alt = this.alt;
+    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
+    $(this).parent().attr('rel', 'gallery').fancybox({
+      helpers : {
+        title: {type: 'inside'}
+      }
+    });
+  });
+});
